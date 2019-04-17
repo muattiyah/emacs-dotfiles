039b5e40d0b6d34648446262bf0cacc064ff2ab0
Muhammad Attiyah <mu.attiyah@gmail.com>
2019-04-17 23:56:13 +0200

    Style fixes.

--- custom/setup-editing.el
+++ custom/setup-editing.el
@@ -11,10 +11,10 @@
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 (add-hook 'ibuffer-hook
-    (lambda ()
-      (ibuffer-projectile-set-filter-groups)
-      (unless (eq ibuffer-sorting-mode 'alphabetic)
-        (ibuffer-do-sort-by-alphabetic))))
+          (lambda ()
+            (ibuffer-projectile-set-filter-groups)
+            (unless (eq ibuffer-sorting-mode 'alphabetic)
+              (ibuffer-do-sort-by-alphabetic))))
 
 (require 'asm-mode)
 (add-hook 'asm-mode-hook (lambda ()
--- custom/setup-rcirc.el
+++ custom/setup-rcirc.el
@@ -1,19 +1,19 @@
 (setq rcirc-server-alist '(("irc.freenode.net"
-														:port 6697
-														:channels ("##math" "##crypto" "##physics" "#haskell" "##opengl" "#lisp" "##electronics" "#clojure" "#clojurescript")
-														:encryption tls)
-													 ;; ("irc.mozilla.org"
-													 ;; 	:port 6697
-													 ;; 	:channels ("#rust" "#rust-gamedev" "#rust-crypto")
-													 ;; 	:encryption tls)
-													 ))
+                            :port 6697
+                            :channels ("##math" "##crypto" "##physics" "#haskell" "##opengl" "#lisp" "##electronics" "#clojure" "#clojurescript")
+                            :encryption tls)
+                           ;; ("irc.mozilla.org"
+                           ;;   :port 6697
+                           ;;   :channels ("#rust" "#rust-gamedev" "#rust-crypto")
+                           ;;   :encryption tls)
+                           ))
 
 
 (setq rcirc-default-nick irc-handle)
 (setq rcirc-default-user-name irc-handle)
 (setq rcirc-default-full-name irc-handle)
 (setq rcirc-authinfo `(("freenode" nickserv ,irc-handle ,freenode-password)
-											 ("moـzilla" nickserv ,irc-handle ,mozilla-password)))
+                       ("moـzilla" nickserv ,irc-handle ,mozilla-password)))
 
 (eval-after-load 'rcirc '(require 'rcirc-notify))
 (eval-after-load 'rcirc '(rcirc-notify-add-hooks))
